7ac04e01c26a8ae28247534d411175efa865248c
zafar-abba-as <zafar@tranzita.com>
2022-05-16 11:57:21 +0530

    first screnn

--- src/js/components/Login/login.js
+++ src/js/components/Login/login.js
@@ -3,6 +3,7 @@ import VtLogo from "../../../images/VTlogo4 3.png"
 import autoEv from "../../../images/apremium-electric-sports-sedan-car-isolated.png"
 import Mail from "../../../images/mail.png"
 import Phone from "../../../images/phone.png"
+import { Checkbox, Typography } from '@mui/material'
 
 function Login() {
     
@@ -15,7 +16,7 @@ function Login() {
             <div style={{marginTop: 90}}>
                <div style={{display: 'flex', flexDirection: 'row', background: '#f4f4f4', width: '80vw', justifyContent: 'space-around', padding: '10px 15px', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', borderRadius: '20px', color:'#000'}}>
                     <div style={{backgroundColor: '#036463', padding: '5px 10px', width: 40, borderRadius: '20px', color:'#fff'}}>
-                    हिन्दी
+                        हिन्दी
                     </div>
                     <div style={{padding: '5px 10px', width: 40, borderRadius: '20px'}}>
                         English
@@ -32,10 +33,13 @@ function Login() {
                     <div style={{width: '324px', height: '48px', borderRadius: '20px', backgroundColor:'#f4f4f4', color: '#036463', textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', display: 'flex'}}> 
                      <img src={Mail} width={20} height={20} style={{marginTop: 17, marginRight: 20, marginLeft: 20}} /> <p>Login With Mobile Number</p>
                     </div>
-                        Or
-
+                        <Typography style={{font:'Montserrat', textAlign: 'center', color: '#036463', fontSize: 12, marginTop: '5px'}}> OR </Typography>
                     <div style={{width: '324px', height: '48px', borderRadius: '20px', backgroundColor:'#f4f4f4', color: '#036463',textAlign: 'center', marginLeft: 'auto', marginRight: 'auto', marginTop: 3, display: 'flex'}}> 
                     <img src={Phone} width={20} height={20} style={{marginTop: 17, marginRight: 20, marginLeft: 20}}/> <p>Login With Mobile Number</p>
+                    </div>
+                    <div style={{display: 'flex', flexDirection: 'row', justifyContent: 'center', alignItems: 'center'}}>
+                        <Checkbox />
+                        <Typography style={{font:'Montserrat', textAlign: 'center', color: '#036463', fontSize: 10}}>I agree to <b>Terms & Conditions</b> and <b>Privacy Policy.</b> </Typography>    
                     </div>
             </div>
             <div style={{display: 'flex', flexDirection: 'column', marginTop: 5, alignItems: 'flex-start', marginTop: 20}}>
