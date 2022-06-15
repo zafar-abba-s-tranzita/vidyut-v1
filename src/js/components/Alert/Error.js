9301cc806ac709b4983899ae2b919cbad0a7cb9a
Zafar Abbas <zafar@tranzita.com>
2022-06-15 23:09:57 +0530

    error handling added for otp and minor ui bugs fixed, email/mobile input validation added++

--- src/js/components/Alert/Error.js
+++ src/js/components/Alert/Error.js
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import Alert from '@mui/material/Alert';
 import Stack from '@mui/material/Stack';
-
+import { COLOR } from '../../../styles/Color'
+ 
 export default function ErrorAlert(props) {
   return (
     <Stack sx={{ width: '100%', mt: props.marginTop }} spacing={2} >
       <Alert variant="outlined" severity="error">
-            { props ? props.errorInfo : '' }
+           <b style={{color: COLOR.TYPO_BASE6}}>  { props ? props.errorInfo : '' } </b>
       </Alert>
     </Stack>
   );
